e129235e9eea0392242e1a765cde5603f8198006
Alven Wang <alvenw@berkeley.edu>
2015-08-10 19:29:36 -0700

    setup server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var Sequelize = require('sequelize');
+sequelize = new Sequelize('courses', 'alven', '1234', {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432
+})
