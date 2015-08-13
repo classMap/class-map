8f8fd30055504244709802d33cde22d1cb04c7da
Anshu Siripurapu <siripura@usc.edu>
2015-08-13 00:13:50 -0700

    Added honors program

--- app/app.js
+++ app/app.js
@@ -5,20 +5,42 @@ var App = angular.module('drag-and-drop', ['ngDragDrop']);
 
 App.controller('formController', function($scope) {
   $scope.major = ""
-  $scope.message = "Congratulations on your admission to USC! \n Let's plan out your curriculum over the next four years. \n Begin by selecting a major."
+  $scope.genEd = ""
+  $scope.message = "Congratulations on your admission to USC! \n Let's plan out your curriculum over the next four years. \n Begin by selecting a major and a general education program."
   $scope.showForm = false;
+  $scope.showReqs = true;
   $scope.addMajor = function(){
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-A</div>').draggable().appendTo("#form");
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-B</div>').draggable().appendTo("#form");
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-B</div>').draggable().appendTo("#form");
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-C</div>').draggable().appendTo("#form");
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-C</div>').draggable().appendTo("#form");
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-D</div>').draggable().appendTo("#form");
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-E</div>').draggable().appendTo("#form");
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-F</div>').draggable().appendTo("#form");
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-G</div>').draggable().appendTo("#form");
-    $('<div class = "course" data-drag="true" data-drop = "true" data-jqyoui-options = "{revert: \'invalid\'}" jqyoui-draggable="{animate: true}">GE-H</div>').draggable().appendTo("#form");
-    $scope.message = "Effective Fall 2015, all undergraduates at USC must satisfy the following degree requirements. Add the GE classes below to your schedule."
-    $scope.showForm = true;
+    if ($scope.genEd === "regular"){
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-A</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-B</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-B</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-C</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-C</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-D</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-E</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-F</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-G</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-H</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $scope.message = "Effective Fall 2015, all undergraduates at USC must satisfy the following degree requirements. Drag the classes below to your schedule."
+  } else if ($scope.genEd = 'honors'){
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 101</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 102</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 103</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 104</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 111</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 112</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-A</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-C</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-F</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-E</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+    $scope.message = "Effective Fall 2015, all Thematic Option students must satisfy the following degree requirements. Drag the classes below to your schedule."
+  }
+  $scope.showForm = true;
+  $scope.showReqs = false;
+  }
+  $scope.addReqs = function(){
+    $scope.message = "Nice! Now let's add your major classes."
+    $scope.showReqs = true;
+
   }
 });
