7b07d259387935e4bff34cf708840bdb3c28349b
Alven Wang <alvenw@berkeley.edu>
2015-08-14 11:24:54 -0700

    merge

--- app/app.js
+++ app/app.js
@@ -8,32 +8,30 @@ App.controller('formController', function($scope) {
   $scope.showReqs = true;
   $scope.addMajor = function(){
     if ($scope.genEd === "regular"){
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-A</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-B</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-B</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-C</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-C</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-D</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-E</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-F</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-G</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-H</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $scope.message = "Effective Fall 2015, all undergraduates at USC must satisfy the following degree requirements. Drag the classes below to your schedule."
-  } else if ($scope.genEd = 'honors'){
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 101</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 102</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 103</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 104</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 111</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 112</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-A</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-C</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-F</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-E</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
-    $scope.message = "Effective Fall 2015, all Thematic Option students must satisfy the following degree requirements. Drag the classes below to your schedule."
-  }
-  $scope.showForm = true;
-  $scope.showReqs = false;
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-A</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-B</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-C</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-D</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-E</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-F</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-G</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-H</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $scope.message = "Effective Fall 2015, all undergraduates at USC must satisfy the following degree requirements. Drag the classes below to your schedule."
+    } else if ($scope.genEd = 'honors'){
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 101</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 102</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 103</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 104</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 111</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 112</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-A</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-C</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-F</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-E</div>').draggable({revert: 'invalid', snap: '.gridItem'}).appendTo("#dropzone");
+      $scope.message = "Effective Fall 2015, all Thematic Option students must satisfy the following degree requirements. Drag the classes below to your schedule."
+    }
+    $scope.showForm = true;
+    $scope.showReqs = false;
   }
   $scope.addReqs = function(){
     $scope.message = "Nice! Now let's add your major classes."
--- server/server.js
+++ server/server.js
@@ -1,31 +1,52 @@
-var http = require('http');
-var express = require('express');
+// var http = require('http');
+// var express = require('express');
+// var app = express();
+// var Sequelize = require('sequelize');
+// sequelize = new Sequelize("process.env.HEROKU_POSTGRESQL_URL")
+
+// var Course = sequelize.define('economics', {
+//   id: Sequelize.INTEGER,
+//   dept: Sequelize.STRING,
+//   num: Sequelize.INTEGER,
+//   name: Sequelize.STRING,
+//   req: Sequelize.BOOLEAN
+//   }, 
+//   {timestamps: false}
+// );
+
+// app.use('/', express.static("client"));
+
+// app.get('/classes', function (req, res, next) {
+//   Course.findAll({
+//     where: {
+//       id: 01
+//     }
+//   }).then(function (economics) {
+//     res.send(economics)
+//   });
+// });
+
+
+
+// app.listen(3000);
+
+var express = require('express')
+  , http    = require('http')
+  , path    = require('path')
+  , db      = require('./models');
+
 var app = express();
-var Sequelize = require('sequelize');
-sequelize = new Sequelize("postgres://alven:1234@localhost:5432/courses")
 
-var Course = sequelize.define('economics', {
-  id: Sequelize.INTEGER,
-  dept: Sequelize.STRING,
-  num: Sequelize.INTEGER,
-  name: Sequelize.STRING,
-  req: Sequelize.BOOLEAN
-  }, 
-  {timestamps: false}
-);
-
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.use('/', express.static("client"));
 
-app.get('/classes', function (req, res, next) {
-  Course.findAll({
-    where: {
-      id: 01
-    }
-  }).then(function (economics) {
-    res.send(economics)
+
+
+db.sequelize.sync().then(function() {
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
   });
 });
-
-
-
-app.listen(3000);
