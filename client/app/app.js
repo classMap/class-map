e1653ca5a6c99c17faa5a807831bc70f2c89d279
Anshu Siripurapu <siripura@usc.edu>
2015-08-15 16:19:44 -0700

    Changed heading, added majors

--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,7 @@ var App = angular.module('main', ['ngDragDrop'])
 App.controller('formController', function($scope, $http) {
   $scope.major = ""
   $scope.genEd = ""
-  $scope.message = "Congratulations on your admission to USC! \n Let's plan out your curriculum over the next four years. \n Begin by selecting a major and a general education program."
+  $scope.message = "Congratulations on your admission to the USC Dornsife College of Letters, Arts and Sciences! \n Let's plan out your curriculum over the next four years. \n Begin by selecting a major and a general education program."
   $scope.showForm = false;
   $scope.showReqs = true;
   $scope.showElec = true;
@@ -43,7 +43,7 @@ App.controller('formController', function($scope, $http) {
     $('<p class = "geinfo"> Further information about the General Education program can be found at <a href= "http://dornsife.usc.edu/2015ge/2015ge-requirements/"> www.dornsife.usc.edu/2015ge/2015ge-requirements/ </a></p>').appendTo("#form");
     $('<p class = "geinfo"> NOTE: All freshmen must enroll in a General Education Seminar (GSEM) in their first year which satisfies one GE requirement. Here, it is satisfying the GE-A requirement.</p>').appendTo("#form");
 
-    $scope.message = "Effective Fall 2015, all undergraduates at USC must satisfy the following degree requirements. Drag the classes below to your schedule. Drag things to the trash can to delete them."
+    $scope.message = "Effective Fall 2015, all undergraduates at USC must satisfy the following degree requirements. Drag the classes below to your schedule. If you have already satisfied a requirement, you can drag things to the trash can to delete them."
     //creates honors program courses
   } else if ($scope.genEd = 'honors'){
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 101</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
