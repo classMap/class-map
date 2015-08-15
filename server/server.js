de08a872d9b9fd462e9d8e40228c303e4d19d47e
Anshu Siripurapu <siripura@usc.edu>
2015-08-15 09:47:35 -0700

    Added heroku port

--- server/server.js
+++ server/server.js
@@ -1,34 +1,4 @@
-// var http = require('http');
-// var express = require('express');
-// var app = express();
-// var Sequelize = require('sequelize');
-// sequelize = new Sequelize("process.env.HEROKU_POSTGRESQL_URL")
 
-// var Course = sequelize.define('economics', {
-//   id: Sequelize.INTEGER,
-//   dept: Sequelize.STRING,
-//   num: Sequelize.INTEGER,
-//   name: Sequelize.STRING,
-//   req: Sequelize.BOOLEAN
-//   },
-//   {timestamps: false}
-// );
-
-// app.use('/', express.static("client"));
-
-// app.get('/classes', function (req, res, next) {
-//   Course.findAll({
-//     where: {
-//       id: 01
-//     }
-//   }).then(function (economics) {
-//     res.send(economics)
-//   });
-// });
-
-
-
-// app.listen(3000);
 
 var express = require('express');
 var mongoose = require('mongoose');
@@ -37,7 +7,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
-mongoose.connect('mongodb://alven:4nshuman@ds033143.mongolab.com:33143/classmap:5000', function(err) {
+mongoose.connect('mongodb://alven:4nshuman@ds033143.mongolab.com:33143/classmap', function(err) {
     if(err){ return err; }
     console.log('connected to DB');
 });
@@ -66,6 +36,8 @@ app.post('/majors', function(req, res){
   })
 });
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000)
+
+
 
 module.exports = app;
