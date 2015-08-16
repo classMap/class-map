6d27c15e1776d4810aadf9fadd3c7bf4ead91dba
Anshu Siripurapu <siripura@usc.edu>
2015-08-15 17:15:29 -0700

    Added information about TO

--- client/app/app.js
+++ client/app/app.js
@@ -40,6 +40,7 @@ App.controller('formController', function($scope, $http) {
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-F</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-G</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-H</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
+    $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Diversity</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
     $('<p class = "geinfo"> Further information about the General Education program can be found at <a href= "http://dornsife.usc.edu/2015ge/2015ge-requirements/"> www.dornsife.usc.edu/2015ge/2015ge-requirements/ </a></p>').appendTo("#form");
     $('<p class = "geinfo"> NOTE: All freshmen must enroll in a General Education Seminar (GSEM) in their first year which satisfies one GE requirement. Here, it is satisfying the GE-A requirement.</p>').appendTo("#form");
 
@@ -56,6 +57,9 @@ App.controller('formController', function($scope, $http) {
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-C</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-F</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-E</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
+    $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Diversity</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
+    $('<p class = "geinfo"> Further information about the Thematic Option program can be found at <a href= "http://dornsife.usc.edu/thematic-option/"> www.dornsife.usc.edu/thematic-option/ </a></p>').appendTo("#form");
+    $('<p class = "geinfo"> NOTE: GE-A, GE-C, GE-E, and GE-F can be satisfied with Advanced Placement or IB credit.</p>').appendTo("#form");
     $scope.message = "Effective Fall 2015, all Thematic Option students must satisfy the following degree requirements. Drag the classes below to your schedule. Drag things to the trash can to delete them."
   }
   $scope.showForm = true;
