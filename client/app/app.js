7ce29ee898e0bc3ce3e4278beb50e6f83f060eae
Anshu Siripurapu <siripura@usc.edu>
2015-08-18 22:24:39 -0700

    Changed fonts and made trash appear after page load

--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,7 @@ var App = angular.module('main', ['ngDragDrop'])
 App.controller('formController', function($scope, $http) {
   $scope.major = ""
   $scope.genEd = ""
-  $scope.message = "Congratulations on your admission to the USC Dornsife College of Letters, Arts and Sciences! \n Let's plan out your curriculum over the next four years. \n Begin by selecting a major and a general education program."
+  $scope.message = "Congratulations on your admission to the USC Dornsife College of Letters, Arts and Sciences!" + "\n" + "\n" + "Let's plan out your curriculum over the next four years." + "\n" + "\n" + "Begin by selecting a major and a general education program."
   $scope.showForm = false;
   $scope.showReqs = true;
   $scope.showElec = true;
@@ -13,6 +13,16 @@ App.controller('formController', function($scope, $http) {
       location.reload();
     }
     //activates the trash can
+    $('#trash').css({
+      'height' : '155px',
+      'width' : '107px',
+      'position': 'absolute',
+      'top' : '540px',
+      'left' : '550px',
+      'background' : 'url("./assets/trash-can.png") no-repeat',
+      'background-size' : 'cover'
+    });
+
     $('#trash').droppable({
           drop: function(event, ui) {
               $(ui.draggable).remove();
@@ -40,11 +50,10 @@ App.controller('formController', function($scope, $http) {
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-F</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-G</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-H</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
-    $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Diversity</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
     $('<p class = "geinfo"> Further information about the General Education program can be found at <a href= "http://dornsife.usc.edu/2015ge/2015ge-requirements/"> www.dornsife.usc.edu/2015ge/2015ge-requirements/ </a></p>').appendTo("#form");
     $('<p class = "geinfo"> NOTE: All freshmen must enroll in a General Education Seminar (GSEM) in their first year which satisfies one GE requirement. Here, it is satisfying the GE-A requirement.</p>').appendTo("#form");
 
-    $scope.message = "Effective Fall 2015, all undergraduates at USC must satisfy the following degree requirements. Drag the classes below to your schedule. If you have already satisfied a requirement, you can drag things to the trash can to delete them."
+    $scope.message = "Effective Fall 2015, all undergraduates at USC must satisfy the following degree requirements." + "\n" + "\n" + "Drag the classes below to your schedule. If you have already satisfied a requirement, you can drag things to the trash can to delete them."
     //creates honors program courses
   } else if ($scope.genEd = 'honors'){
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">Core 101</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
@@ -59,7 +68,7 @@ App.controller('formController', function($scope, $http) {
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-E</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).appendTo("#dropzone");
     $('<p class = "geinfo"> Further information about the Thematic Option program can be found at <a href= "http://dornsife.usc.edu/thematic-option/"> www.dornsife.usc.edu/thematic-option/ </a></p>').appendTo("#form");
     $('<p class = "geinfo"> NOTE: GE-A, GE-C, GE-E, and GE-F can be satisfied with Advanced Placement or IB credit.</p>').appendTo("#form");
-    $scope.message = "Effective Fall 2015, all Thematic Option students must satisfy the following degree requirements. Drag the classes below to your schedule. Drag things to the trash can to delete them."
+    $scope.message = "Effective Fall 2015, all Thematic Option students must satisfy the following degree requirements." + "\n" + "\n" + "Drag the classes below to your schedule. Drag things to the trash can to delete them."
   }
   $scope.showForm = true;
   $scope.showReqs = false;
