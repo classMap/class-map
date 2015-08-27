a07bd96e5491e016ebdcfe1650215f98d1569764
Anshu Siripurapu <siripura@usc.edu>
2015-08-27 12:24:14 -0700

    Added unit counter

--- client/app/app.js
+++ client/app/app.js
@@ -7,18 +7,27 @@ App.controller('formController', function($scope, $http) {
   $scope.showForm = false;
   $scope.showReqs = true;
   $scope.showElec = true;
+  $scope.units = 0;
   $scope.addMajor = function(){
     if (!$scope.major || !$scope.genEd){
       alert("Sorry, you need to select a major and a GE program");
       location.reload();
     }
+    //creates title with user's major
+    $('<div id = "majorHeader"></div>').append("Your major is: " + $scope.major).fadeIn(1000).appendTo('body');
+
 
     //activates the trash can
     $('<div id = "trash"></div>').fadeIn(1000).appendTo('body');
 
+
     $('#trash').droppable({
           drop: function(event, ui) {
               $(ui.draggable).remove();
+              $scope.units-=4;
+              $("#units").remove();
+              $('<div id = "units"></div>').append("Units earned: " +$scope.units).appendTo('body');
+
           }
       });
     //sends request to server which returns classes array for user's major, iterates over array to create course divs
@@ -28,6 +37,9 @@ App.controller('formController', function($scope, $http) {
       for (var i=0; i<classes.length; i++){
         var text = classes[i];
         $('<div class = "course" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}"></div>').append(text).draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).fadeIn(1000).appendTo("#"+(i+1));
+        $scope.units+=4;
+        $("#units").remove();
+        $('<div id = "units"></div>').append("Units earned: " +$scope.units).appendTo('body');
       }
     });
 
@@ -45,7 +57,6 @@ App.controller('formController', function($scope, $http) {
     $('<div class = "ge" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}">GE-H</div>').draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).fadeIn(1000).appendTo("#dropzone");
     $('<p class = "geinfo"> Further information about the General Education program can be found at <a href= "http://dornsife.usc.edu/2015ge/2015ge-requirements/"> www.dornsife.usc.edu/2015ge/2015ge-requirements/ </a></p>').appendTo("#form");
     $('<p class = "geinfo"> NOTE: All freshmen must enroll in a General Education Seminar (GSEM) in their first year which satisfies one GE requirement. Here, it is satisfying the GE-A requirement.</p>').appendTo("#form");
-
     $scope.message = "Effective Fall 2015, all undergraduates at USC must satisfy the following degree requirements." + "\n" + "\n" +"Drag the classes below to your schedule. If you have already satisfied a requirement, you can drag things to the trash can to delete them."
     //creates honors program courses
   } else if ($scope.genEd = 'honors'){
@@ -64,6 +75,9 @@ App.controller('formController', function($scope, $http) {
     $scope.message = "Effective Fall 2015, all Thematic Option students must satisfy the following degree requirements." + "\n" + "\n" + "Drag the classes below to your schedule. Drag things to the trash can to delete them."
   }
 
+  $scope.units = 40;
+  $("#units").remove();
+  $('<div id = "units"></div>').append("Units earned: " +$scope.units).fadeIn(1000).appendTo('body');
 
   $scope.showForm = true;
   $scope.showReqs = false;
@@ -85,22 +99,45 @@ App.controller('formController', function($scope, $http) {
     if (elec1){
       $('<div class = "elective" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}"></div>').append(elec1).draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).fadeIn(1000).appendTo("#dropzone");
       $scope.elec1 = "";
+      $scope.units+=4;
+      $("#units").remove();
+      $('<div id = "units"></div>').append("Units earned: " +$scope.units).appendTo('body');
     }
     if (elec2){
       $('<div class = "elective" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}"></div>').append(elec2).draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).fadeIn(1000).appendTo("#dropzone");
       $scope.elec2 = "";
+      $scope.units+=4;
+      $("#units").remove();
+      $('<div id = "units"></div>').append("Units earned: " +$scope.units).appendTo('body');
+
+
     }
     if (elec3){
       $('<div class = "elective" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}"></div>').append(elec3).draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).fadeIn(1000).appendTo("#dropzone");
       $scope.elec3 = "";
+      $scope.units+=4;
+      $("#units").remove();
+      $('<div id = "units"></div>').append("Units earned: " +$scope.units).appendTo('body');
+
+
     }
     if (elec4){
       $('<div class = "elective" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}"></div>').append(elec4).draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).fadeIn(1000).appendTo("#dropzone");
       $scope.elec4 = "";
+      $scope.units+=4;
+      $("#units").remove();
+      $('<div id = "units"></div>').append("Units earned: " +$scope.units).appendTo('body');
+
+
     }
     if (elec5){
       $('<div class = "elective" data-drag="true" data-drop = "true"  jqyoui-draggable="{animate: true}"></div>').append(elec5).draggable({revert: 'invalid', snap: '.gridItem', snapTolerance: 50}).fadeIn(1000).appendTo("#dropzone");
       $scope.elec5 = "";
+      $scope.units+=4;
+      $("#units").remove();
+      $('<div id = "units"></div>').append("Units earned: " +$scope.units).appendTo('body');
+
+
     }
   }
 });
